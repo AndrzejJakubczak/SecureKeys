452991512f729ad0fd589477c661a282a628747b
Andrzej Jakubczak <121877493+AndrzejJakubczak@users.noreply.github.com>
2023-06-15 23:09:49 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import './App.css';
+
+import Checkbox from './components/Checkbox';
+
+function App() {
+  const [passwordGen, setPasswordGen] = useState({
+    length: 5,
+    uppercase: false,
+    lowercase: false,
+    numbers: false,
+    symbols: false,
+  });
+  const [handelText, setHandelText] = useState('');
+  const [copied, setCopied] = useState(false);
+
+  const handleChangeUppercase = () => {
+    setPasswordGen({
+      ...passwordGen,
+      uppercase: !passwordGen.uppercase,
+    });
+  };
+
+  const handleChangeLowercase = () => {
+    setPasswordGen({
+      ...passwordGen,
+      lowercase: !passwordGen.lowercase,
+    });
+  };
+
+  const handleChangeNumbers = () => {
+    setPasswordGen({
+      ...passwordGen,
+      numbers: !passwordGen.numbers,
+    });
+  };
+
+  const handleChangeSymbols = () => {
+    setPasswordGen({
+      ...passwordGen,
+      symbols: !passwordGen.symbols,
+    });
+  };
+
+  const setPasswordLength = (val) => {
+    setPasswordGen({
+      ...passwordGen,
+      length: val,
+    });
+  };
