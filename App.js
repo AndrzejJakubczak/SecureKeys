b3fc6a47a5574bed80684d17c1b7e5c609945634
Andrzej Jakubczak <121877493+AndrzejJakubczak@users.noreply.github.com>
2023-07-04 10:02:56 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -48,3 +48,137 @@ function App() {
       length: val,
     });
   };
+
+  function generatePassword() {
+    const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
+    const characterCodes = Array.from(Array(26)).map((_e, i) => i + 97);
+    const lowerCaseLetters = characterCodes.map((code) =>
+      String.fromCharCode(code)
+    );
+    const upperCaseLetters = lowerCaseLetters.map((letter) =>
+      letter.toUpperCase()
+    );
+
+    const { length, uppercase, lowercase, numbers, symbols } = passwordGen;
+
+    const generateTheWord = (
+      length,
+      uppercase,
+      lowercase,
+      numbers,
+      symbols
+    ) => {
+      const availableCharacters = [
+        ...(lowercase ? lowerCaseLetters : []),
+        ...(uppercase ? upperCaseLetters : []),
+        ...(numbers ? numbersArray : []),
+        ...(symbols ? symbolsArray : []),
+      ];
+      const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+      const characters = shuffleArray(availableCharacters).slice(0, length);
+      setHandelText(characters.join(''));
+      return characters;
+    };
+
+    generateTheWord(length, uppercase, lowercase, numbers, symbols);
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="container wrapper-box">
+        <h2 className="title-box">Generator haseł</h2>
+        <div className="password-box">
+          <input
+            type="text"
+            value={handelText}
+            placeholder="Twoje hasło"
+            autoComplete="off"
+            onChange={(e) => setHandelText(e.target.value)}
+          />
+          <button
+            className="copy-button"
+            onClick={() => {
+              if (handelText.length > 0) {
+                navigator.clipboard.writeText(handelText);
+                setCopied(true);
+                setInterval(() => {
+                  setCopied(false);
+                }, 2000);
+              }
+            }}
+          >
+            {copied ? 'Skopiowane!' : 'Kopiuj'}
+          </button>
+        </div>
+        <br />
+        <div className="word-crieteria__box">
+          <div>
+            <label>Długość hasła</label>
+          </div>
+          <div>
+            <input
+              type="number"
+              min="4"
+              max="20"
+              value={passwordGen.length}
+              onChange={(e) => setPasswordLength(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Dołącz do hasła wielkie litery</label>
+          </div>
+          <div>
+            <Checkbox
+              value={passwordGen.uppercase}
+              onChange={handleChangeUppercase}
+            />
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Dołącz do hasła małe litery</label>
+          </div>
+          <div>
+            <Checkbox
+              value={passwordGen.lowercase}
+              onChange={handleChangeLowercase}
+            />
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Dołącz do hasła cyfry</label>
+          </div>
+          <div>
+            <Checkbox
+              value={passwordGen.numbers}
+              onChange={handleChangeNumbers}
+            />
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Dołącz do hasła symbole</label>
+          </div>
+          <div>
+            <Checkbox
+              value={passwordGen.symbols}
+              onChange={handleChangeSymbols}
+            />
+          </div>
+        </div>
+        <div>
+          <button className="generate-button" onClick={generatePassword}>
+            Wygeneruj hasło
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
